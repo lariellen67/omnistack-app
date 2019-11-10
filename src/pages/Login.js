99d378baff9f434d476916439eb1b076e523748c
Larissa <lariellen67@gmail.com>
2019-11-10 17:12:12 -0300

    'tela_listagem_de_devs_app'

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage'
 import {View, Text, StyleSheet, Image, TextInput, TouchableOpacity} from 'react-native';
+import api from '../services/api';
 import icon from '../images/icon.png';
 
-export default function Login(){
+export default function Login({ navigation }){
+    const [user, setUser] = useState('');
+
+    //useEffect dispara funcionalidades assim que o componente é exibido em tela
+    useEffect(() => { //vai disparar apenas uma vez ('[]'), depois que uma variável x for executada
+        AsyncStorage.getItem('user').then(user=>{ //vai pegar o item user, then --> se tiver algum resultado 
+            if(user){ //se tiver alguma info dentro do usuário, manda ele direto pra tela Main
+                navigation.navigate('Main', {user})
+            }
+        })
+    }, []);
+
+    async function handleLogin() {
+
+        const response = await api.post('/devs', {username: user});
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id); //chamada para o storage local dentro do app
+
+        navigation.navigate('Main', { user: _id });
+    }
+
     return(
         <View style={styles.container}>
             <Image source={icon}></Image>
@@ -11,10 +34,13 @@ export default function Login(){
                 autoCorrect={false}
                 placeholder="Digite seu usuário do GitHub" 
                 placeholderTextColor="#888888"
-                style={styles.input}>
+                style={styles.input}
+                value={user}
+                onChangeText={setUser}
+                >
             </TextInput>
 
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
                 <Text style={styles.buttonText}>Entrar</Text>
             </TouchableOpacity>
         </View>
---
+++ src/pages/Main.js
@@ -0,0 +1,191 @@
+import React, {useEffect, useState} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {SafeAreaView, Text, Image, StyleSheet, View, TouchableOpacity} from 'react-native';
+import api from '../services/api';
+import icon from '../images/icon.png';
+import like from '../images/like.png';
+import deslike from '../images/deslike.png';
+
+export default function Main({ navigation }){
+    const id = navigation.getParam('user');
+    const [users, setUsers] = useState([]); 
+
+    console.log(id);
+
+    useEffect(() => {
+        async function loadUsers(){ 
+            const response = await api.get('/devs', { 
+                headers: { 
+                    user: id,
+                }
+            })
+            setUsers(response.data); 
+        }
+        loadUsers(); 
+    }, [id]);
+
+    async function handleDeslike(){
+        const [user, ...rest] = users;
+
+        await api.post(`/devs/${user._id}/deslikes`, null, {
+            headers: {user: id},
+        })
+        setUsers(rest);
+        /*setUsers(users.filter(user => user._id !== id));*/ //não é mais necessário, é substituído pelo de cima
+    }
+
+    async function handleLike(){
+        const [user, ...rest] = users;
+        await api.post(`/devs/${user._id}/likes`, null, {
+            headers: {user: id},
+        })
+        setUsers(rest);
+    }
+
+    async function handleLogout() {
+        await AsyncStorage.clear();
+        navigation.navigate('Login');
+    }
+
+    return(
+        <SafeAreaView style={styles.container}>
+            <View>
+            <TouchableOpacity style={styles.buttonlo} onPress={handleLogout}>
+                    <Text style={styles.buttonloText}>Logout</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Image style={styles.icon} source={icon}></Image>
+
+            <View style={styles.cardsContainer}>
+               {users.length > 0 ? 
+                    users.map((user, index) => (
+                        <View key={user._id} style={[styles.card, {zIndex: users.length - index }]}>
+                            <Image style={styles.avatar} source={{ uri: user.avatar}}></Image>
+                            <View style={styles.footer}>
+                                <Text style={styles.name}>{user.name}</Text>
+                                <Text style={styles.bio} numberOfLines={4}>{user.bio}</Text>
+                            </View>
+                        </View>
+                    ))           
+                : (
+                    <Text style={styles.empty}>Acabou, you're lonely :( </Text>
+                )}
+            </View>
+
+            {users.length > 0 && (
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={styles.button} onPress={handleDeslike}>
+                    <Image source={deslike}></Image>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleLike}>
+                    <Image source={like}></Image>
+                </TouchableOpacity>
+            </View>
+            )}
+
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+
+    icon:{
+        marginTop: -60,
+    },
+
+    empty:{
+        alignSelf: 'center',
+        color: '#999',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },  
+
+    cardsContainer:{
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        maxHeight: 500
+    },
+
+    card:{
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 8,
+        margin: 30,
+        marginBottom: -10, //se der merda no estilo, remove isso
+        overflow: 'hidden',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+
+    },
+
+    avatar:{
+        flex: 1,
+        height: 300,
+    },
+
+    footer:{
+        backgroundColor: '#fff',
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+    },
+
+    name:{
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333'
+    },
+
+    bio:{
+        fontSize: 14,
+        color: '#999',
+        marginTop: 5,
+        lineHeight: 18
+    },
+
+    buttonsContainer:{
+        flexDirection: 'row',
+        marginBottom: 30,
+    },
+
+    button:{
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        backgroundColor: '#f8f8ff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        elevation: 2,
+    },
+
+    buttonlo:{
+        width: 70,
+        height: 40,
+        borderRadius: 10,
+        backgroundColor: '#df4723',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 50,
+        marginRight: -300,
+        marginLeft: 115,
+        marginTop: 10,
+        elevation: 2,
+    },
+
+    buttonloText:{
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 14,
+    },
+});
